c05f91a63278db7a36df0fc1d740d24b56d96f01
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2025-03-12 10:54:45 +0200

    Add Passenger class with properties for booking system
    Introduced a new `Passenger` class in the `Airport_Ticket_Booking_System.Models` namespace. The class includes properties for `Name` and `PassportNumber`. Added necessary `using` directives at the top of the file.

---
+++ Models/Passenger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking_System.Models
+{
+    class Passenger
+    {
+        public string Name { get; set; }
+        public string PassportNumber { get; set; }
+    }
+}
