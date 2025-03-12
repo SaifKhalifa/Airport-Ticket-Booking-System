a1dfaa75b80156fde80c8e939918f88c0d08be90
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2025-03-12 10:53:14 +0200

    Add Flight class for flight details and pricing
    Introduces a new `Flight` class in the
    `Airport_Ticket_Booking_System.Models` namespace.
    This class includes properties for flight number,
    departure and destination countries, airports,
    departure date, and pricing for Economy,
    Business, and First Class tickets.

---
+++ Models/Flight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking_System.Models
+{
+    class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string DepartureCountry { get; set; }
+        public string DestinationCountry { get; set; }
+        public string DepartureAirport { get; set; }
+        public string ArrivalAirport { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public decimal EconomyPrice { get; set; }
+        public decimal BusinessPrice { get; set; }
+        public decimal FirstClassPrice { get; set; }
+    }
+}
