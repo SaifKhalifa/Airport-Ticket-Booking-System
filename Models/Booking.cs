2474ef9602f8d57306844d68e45c3bde425b0366
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2025-03-12 10:55:35 +0200

    Add Booking class for managing flight reservations
    Introduces the `Booking` class in the `Airport_Ticket_Booking_System.Models` namespace. This class includes properties for `BookingId`, `PassengerName`, `PassportNumber`, `FlightNumber`, `Class`, and `Price`, facilitating the management of booking details.

---
+++ Models/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking_System.Models
+{
+    class Booking
+    {
+        public string BookingId { get; set; }
+        public string PassengerName { get; set; }
+        public string PassportNumber { get; set; }
+        public string FlightNumber { get; set; }
+        public string Class { get; set; } // Economy, Business, FirstClass
+        public decimal Price { get; set; }
+    }
+}
